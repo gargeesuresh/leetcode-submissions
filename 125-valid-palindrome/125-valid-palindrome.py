b89bd97617a2da130136088750bf538ba0fc17ca
gargee suresh <gargee.suresh@gmail.com>
2022-08-24 12:20:30 +0530

    Time: 49 ms (91.89%), Space: 14.4 MB (85.98%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        i=0
+        while(i<len(s)):
+            if (s[i].isalnum()):
+                i+=1
+                pass
+            else:
+                s=s.replace(s[i],"")
+            
+        s=s.replace(" ","")
+        if(s==s[::-1]):
+            return True
+        return False
+        
