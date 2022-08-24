79dec69e0d947b553bdc800fdde478dec0827cff
gargee suresh <gargee.suresh@gmail.com>
2022-08-24 19:39:42 +0530

    Time: 87 ms (19.50%), Space: 14.2 MB (45.83%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -3,10 +3,6 @@ class Solution:
         s_map={}
         t_map={}
         for i in range(len(s)):
-            if(s[i] in s_map and t[i] not in t_map):
-                return False
-            if(t[i] in t_map and s[i] not in s_map):
-                return False
             if (s[i] in s_map and s_map[s[i]]!=t[i]):
                 return False
             if (t[i] in t_map and t_map[t[i]]!=s[i]):
@@ -14,5 +10,7 @@ class Solution:
             if (s[i] not in s_map and t[i] not in t_map):
                 s_map[s[i]]=t[i]
                 t_map[t[i]]=s[i]
+            if((s[i] in s_map and t[i] not in t_map) or (t[i] in t_map and s[i] not in s_map)):
+                return False
         return True
                 
