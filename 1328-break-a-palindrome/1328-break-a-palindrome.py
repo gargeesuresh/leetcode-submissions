db9bf22e5b37934f5f4f991e25810d404219466b
gargee suresh <gargee.suresh@gmail.com>
2022-08-25 15:17:47 +0530

    Time: 64 ms (8.89%), Space: 14 MB (16.23%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -5,10 +5,10 @@ class Solution:
         if(s[0]!="a"):
             return "a"+s[1:]
         i=0
-        while(s[i]=="a"):
-            if(i==len(s)-1):
-                return s[:-1]+"b"
+        while(i<len(s) and s[i]=="a"):
             i+=1
+        if(i==len(s)):
+            return s[:-1]+"b"
         if(len(s)%2==1 and int((len(s)-1)/2)==i):
             return s[:-1]+"b"
         return s[:i]+"a"+s[i+1:]
