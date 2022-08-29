ea2a4a138b603a912f529094465a3008f1a2216f
gargee suresh <gargee.suresh@gmail.com>
2022-08-29 23:26:56 +0530

    Time: 306 ms (38.02%), Space: 15.5 MB (96.95%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    # in-place
+    def moveZeroes(self, nums):
+        zero = 0  # records the position of "0"
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
